58fa42d74843a6f2bd79cf3ab63da185e3bfc882
Thanakon <thanakon.piti@gmail.com>
2024-09-27 10:21:42 +0700

    feat: add font in tour-package section

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    content: [
+        './travel-company.html',
+    ],
+    theme: {
+        extent: {
+            fontFamily: { Google:["Poppins"],},
+            fontFamily: { Google:["DM+Sans"],},
+        },
+    }
+}
